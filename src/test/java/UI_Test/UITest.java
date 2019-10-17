1d4b0fb6053fb92a2c417b1bd3c9edb79039140e
YakuninaNadya <43966610+YakuninaNadya@users.noreply.github.com>
2019-10-17 19:04:41 +0300

    Update UITest.java

--- src/test/java/UI_Test/UITest.java
+++ src/test/java/UI_Test/UITest.java
@@ -24,14 +24,27 @@ public class UITest {
     }
 
     @Test
+    // тест ищет тендер по id и проверяем тот ли тендер был найден
     public void TenderIDSearchTest() {
+        
+        // запускаем браузер и переходим на сайт
         MainPage mainPage = new MainPage(driver);
+        
+        // кликаем по ссылке "процедуры"
         TendersPage tendersPage = mainPage.selectTendersTab();
+        
+        // проверяем, что мы действительно находимся в процедурах
         Assert.assertEquals(tendersPage.getTitle(), "ПРОЦЕДУРЫ");
+        
+        // в поиске вводим id тендера и нажимаем на кнопку "найти"
         tendersPage.enterTenderID("401567");
         tendersPage.clickSearchButton();
+        
+        // получаем количество найденных строк и сверяем его с заданным
         int rowCount = tendersPage.getNumberOfRows();
         Assert.assertEquals(rowCount, 1);
+        
+        // в полученной строке находим id тендера и сверяем его с заданным id
         String id = tendersPage.getID(1);
         Assert.assertEquals(id, "401567");
 
